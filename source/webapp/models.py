a4dde5c7048683b9ed4298fcf2f95f626b079b8e
Artur <metis130383@gmail.com>
2020-08-06 16:48:56 +0600

    выполнил первый этап создал модель , админку, фикстуру

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Status(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название')
+
+    def __str__(self):
+        return f'{self.name}'
+
+class Type(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название')
+
+    def __str__(self):
+        return f'{self.name}'
+
+class Task(models.Model):
+    summary = models.CharField(max_length=100, verbose_name='Заголовок')
+    descriptions = models.TextField(max_length=3000, null=True, blank=True, verbose_name='Описание')
+    status = models.ForeignKey('webapp.Status', on_delete=models.PROTECT, verbose_name='Статус')
+    type =  models.ForeignKey('webapp.Type', on_delete=models.PROTECT, verbose_name='Тип')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+
+    def __str__(self):
+        return f'{self.summary}'
