02dce5f0aaf5dd82c3038df8bd1d02e023b0e28b
Artur <metis130383@gmail.com>
2020-08-06 17:46:26 +0600

    Создал базовый шаблон , главную страницу, просмотр одной  страници

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseNotAllowed
+from django.views.generic import View, TemplateView
+from webapp.models import Task
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = Task.objects.all()
+        return context
+
+
+class TaskView(TemplateView):
+    template_name = 'task_view.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['task'] = get_object_or_404(Task, pk=kwargs['task_id'])
+        return context
 
 # Create your views here.
