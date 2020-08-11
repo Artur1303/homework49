febf2e1aff822c9f4ff2fc16a2b7177da46f0563
Artur <metis130383@gmail.com>
2020-08-11 23:13:14 +0600

    Добавлен валедатор  для  проверки  цензуру

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,8 +1,9 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from django.core.validators import BaseValidator
 from django.utils.deconstruct import deconstructible
-
 from .models import Status, Task, Type
+
 
 @deconstructible
 class MinLengthValidator(BaseValidator):
@@ -25,5 +26,16 @@ class TaskForm(forms.ModelForm):
     # descriptions = forms.CharField(max_length=3000, required=False, label='Описание',widget=forms.Textarea)
     # status = forms.ModelChoiceField(queryset=Status.objects.all(), required=True, label='Статус')
     # type = forms.ModelMultipleChoiceField(queryset=Type.objects.all(), required=True, label='Тип')
+
+    def clean(self):
+            cleaned_data = super().clean()
+            errors = []
+            valid = ('mat','fack','han')
+            descriptions = cleaned_data.get('descriptions')
+            for valids in valid:
+                if descriptions == valids:
+                    errors.append(ValidationError("You used a bad word in the description"))
+                    raise ValidationError(errors)
+            return cleaned_data
 
 
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.core.validators import MinLengthValidator
 
 
+
 class Status(models.Model):
     name = models.CharField(max_length=100, verbose_name='Название')
 
