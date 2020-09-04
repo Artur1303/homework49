5df4676eb60e80bcb1fd7d2668ba71356cbe6b2c
Artur <metis130383@gmail.com>
2020-09-05 01:23:20 +0600

    добавил в форму валидатор, который проверяет, что хотя бы одно из полей (first_name, last_name) заполнено.

--- source/accounts/forms.py
+++ source/accounts/forms.py
@@ -1,7 +1,26 @@
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
 
 
 class MyUserCreationForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super(MyUserCreationForm, self).__init__(*args, **kwargs)
+
+        self.fields['email'].required = True
+
+    def clean(self):
+        cleaned_data = super(MyUserCreationForm, self).clean()
+        last_name = cleaned_data.get("last_name")
+        first_name = cleaned_data.get("first_name")
+
+        if last_name in first_name == '':
+            raise forms.ValidationError("Заполните поли хотябы  одно  поле Last_name или First_name")
+
+        return cleaned_data
+
     class Meta(UserCreationForm.Meta):
         fields = ['username', 'password1', 'password2',
                   'first_name', 'last_name', 'email']
+
+
+
