34e28173ca613a0e5f943edec24157db16350e9e
Artur <metis130383@gmail.com>
2020-09-13 12:39:22 +0600

    Добавленна  модель пользователя

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -1,3 +1,22 @@
+
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user: AbstractUser = models.OneToOneField(get_user_model(), related_name='profile', on_delete=models.CASCADE,
+                                              verbose_name='Пользователь')
+    avatar = models.ImageField(null=True, blank=True, upload_to='user_pics', verbose_name='Аватар')
+    profile_github = models.URLField(max_length=100, null=True, blank=True, verbose_name='Профиль  на гитхаб')
+    about_myself = models.TextField(max_length=2000, null=True, blank=True, verbose_name='О себе', default=None)
+
+
+    def __str__(self):
+        return self.user.get_full_name() + "'s Profile"
+
+    class Meta:
+        verbose_name ='Профиль'
+        verbose_name_plural = 'Профили'
+        permissions = {
+            ('viewing_the_list_of_users', 'просмотр списока пользователей')}
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 from webapp.validators import is_digit, cennz
+from django.db import models
+from django.contrib.auth import get_user_model
 
 
 class Status(models.Model):
@@ -42,3 +44,4 @@ class Project(models.Model):
 
     def __str__(self):
         return f'{self.name}'
+
