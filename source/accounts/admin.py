9036e41a46b96d11bd9e72e973f4a19413581820
Artur <metis130383@gmail.com>
2020-09-13 13:27:27 +0600

    Профил  зарегистрированн  в  админки

--- source/accounts/admin.py
+++ source/accounts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from .models import Profile
 
-# Register your models here.
+
+class ProfileInLine(admin.StackedInline):
+    model = Profile
+    exclude = []
+
+
+class ProfileAdmin(UserAdmin):
+    inlines = [ProfileInLine]
+
+User = get_user_model()
+admin.site.unregister(User)
+admin.site.register(User, ProfileAdmin)
