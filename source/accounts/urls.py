ece88f3dbf732197b3373dc9429361631189d484
Artur <metis130383@gmail.com>
2020-09-17 01:16:11 +0600

    Добавил  редактирование  профиля

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth.views import LogoutView, LoginView
-from accounts.views import RegisterView, UserDetailView, UserList
+from accounts.views import RegisterView, UserDetailView, UserList, UserChangeView
 
 app_name = 'accounts'
 
@@ -10,6 +10,7 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='create'),
     path('<int:pk>/', UserDetailView.as_view(), name='detail'),
-    path('user_list/', UserList.as_view(), name='user_list')
+    path('user_list/', UserList.as_view(), name='user_list'),
+    path('<int:pk>/update/', UserChangeView.as_view(), name="change"),
 
 ]
